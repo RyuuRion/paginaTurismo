03fdeec22eeb46a838af89dda14f929613ed1530
Jordan <aegsyg@gmail.com>
2019-06-24 23:27:51 -0400

    funcionalidades casi al 96%

--- modU/restaurantes/restaurantesP/index.php
+++ modU/restaurantes/restaurantesP/index.php
@@ -21,6 +21,7 @@
     <link rel="stylesheet" type="text/css" href="/css/fontello.css">
     <link rel="stylesheet" type="text/css" href="/css/style2.css">
     <link rel="stylesheet" type="text/css" href="/css/style3.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <title>Inicio</title>
   </head>
   <body>
